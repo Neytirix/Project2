2f34fad29ee7a644c9183e8e5c658610bb283128
Sasha Babayan <sasha44@uw.edu>
2013-04-21 18:05:00 -0700

    Fresh commit
    Please

---
+++ src/GArrayStack.java
@@ -0,0 +1,83 @@
+/* Name: Alexandra (Sasha) Babayan
+ * Section: AB
+ * 4/10/2013
+*/
+
+// Creates a stack for a generic type using an array
+
+import java.util.EmptyStackException;
+
+public class GArrayStack<E> implements GStack<E>{
+	private E[] stack; 
+	private int count; //current number of objects in the stack
+	
+	/*
+	 * Constructs an ArrayStack storing type E
+	 */
+	public GArrayStack(){
+		stack = (E[])new Object[10]; 
+		count = 0;
+	}
+	
+	/*
+	 * Checks to see if the ArrayStack is empty, returns true if the ArrayStack
+	 * is empty otherwise returns false.
+	 */
+	public boolean isEmpty() {
+		if (count == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Takes element d of type E as a parameter and adds it to the ArrayStack 
+	 */
+	public void push(E d) {
+		if(count == stack.length) {
+			stack = doubleArray(stack); 
+		}
+		count++;
+		stack[count-1] = d; 	
+	}
+
+	/*
+	 * Returns the most recent element added to the ArrayStack and removes it.
+	 * Element returned is of type E. Throws EmptyStackException if the 
+	 * ArrayStack is empty.
+	 */
+	public E pop() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		E value = stack[count-1]; 
+		stack[count-1] = null; 
+		count--; 
+		return value;
+	}
+
+	/*
+	 * Returns the most recent element added to the ArrayStack. Returned
+	 * element is of type E. Throws EmptyStackException if the 
+	 * ArrayStack is empty. 
+	 */
+	public E peek() {
+		if (isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return stack[count-1];
+	}
+	
+	/*
+	 * Doubles the length of the generic array passed and copies over all 
+	 * existing elements into the new array, preserving the order. Returns
+	 * the new generic array. 
+	 */
+	private E[] doubleArray(E[] original) {
+		E[] newArray = (E[])new Object[original.length * 2];
+		for(int i = 0; i < original.length; i++) {
+			newArray[i] = original[i];
+		}
+		return newArray;
+	}
+}
---
+++ src/StringComparator.java
@@ -0,0 +1,26 @@
+// Sasha Babayan & Brian Park
+// 4/21/2013
+// Project 2
+
+public class StringComparator implements Comparator<String>{
+
+	@Override
+	public int compare(String e1, String e2) {
+		int length = Math.min(e1.length(), e2.length());
+		for (int i = 0; i < length; i++) {
+			if (e1.charAt(i) > e2.charAt(i)) {
+				return 1;
+			} else if(e1.charAt(i) < e2.charAt(i)) {
+				return -1;
+			}
+		}
+		if (length == e1.length() && length == e2.length()) {
+			return 0;
+		} else if (length == e1.length()) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+}
