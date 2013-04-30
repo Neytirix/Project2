4e7b500c7234160f9c7f2e006001d7ecfbb47764
Brian Park <brokenazn@gmail.com>
2013-04-29 23:17:03 -0700

    Added some tests, broke some things

--- src/MoveToFrontList.java
+++ src/MoveToFrontList.java
@@ -12,7 +12,7 @@ public class MoveToFrontList<E> implements DataCounter<E> {
 	
 	@Override
 	public void incCount(E data) {
-		boolean dataExists = true;
+		boolean dataExists = false;
 		LinkedNode current = front;
 		if(front == null) { //case 1: linked list is empty, sets front node equal to value passed with a count of 1
 			front = new LinkedNode(data, 1);
@@ -20,7 +20,7 @@ public class MoveToFrontList<E> implements DataCounter<E> {
 		} else if(comparator.compare(current.data, data) == 0) { //case 2: the first node is the value passed --> increments its count by 1
 			current.count += 1;	
 		} else { //case 3: searches entire list for value passed
-			while(current.next != null) {
+			while(current != null && current.next != null) {
 				if(comparator.compare(current.next.data, data) == 0) {
 					current.next.count += 1;
 					//moves node to front		
@@ -28,10 +28,11 @@ public class MoveToFrontList<E> implements DataCounter<E> {
 					current.next = current.next.next;
 					temp.next = front;
 					front = temp;
+					dataExists = true;
 				}
 				current = current.next;
 			}
-			dataExists = false;
+			
 		}
 		if (!dataExists) {
 			current = new LinkedNode(data, 1, front);
--- src/TestMoveToFrontList.java
+++ src/TestMoveToFrontList.java
@@ -1,13 +1,125 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestMoveToFrontList {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	private static final int TIMEOUT = 2000;
+	MoveToFrontList<Integer> intMover;
+	
+	@Before
+	public void setUp() throws Exception {
+		intMover = new MoveToFrontList<Integer>(new Comparator<Integer>() {
+			public int compare(Integer e1, Integer e2) {
+				return e1 - e2;
+			};
+		});
 	}
 
+	@Test(timeout = TIMEOUT) 
+	public void sizeEmptyTest() {
+		assertEquals("Empty",0,intMover.getSize());
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void singleIncTest() {
+		intMover.incCount(0);
+		assertNotNull(intMover);
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void hundredIncTest() {
+		for(int x = 0; x < 100; x++)
+			intMover.incCount(0);
+		assertNotNull(intMover);
+	}
+
+	@Test (timeout = TIMEOUT)
+	public void altIncTest() {
+		intMover.incCount(0);
+		intMover.incCount(1);
+		intMover.incCount(0);
+		assertNotNull(intMover);
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void multipleAltIncTest() {
+		for(int x = 0; x<100; x++) {
+			intMover.incCount(0);
+			intMover.incCount(1);
+		}
+		assertNotNull(intMover);
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void multipleIncTest() {
+		for(int x = 0; x<100; x++) {
+			intMover.incCount(0);
+			intMover.incCount(1);
+			intMover.incCount(2);
+			intMover.incCount(3);
+		}
+		assertNotNull(intMover);
+	}
+	
+	public void negativeIncTest() {
+		intMover.incCount(-1);
+		
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void singleSizeTest() {
+		intMover.incCount(0);
+		assertEquals("Single insert size",1,intMover.getSize());
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void doubleSizeTest() {
+		intMover.incCount(0);
+		intMover.incCount(1);
+		assertEquals("Double insert size",2,intMover.getSize());
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void singleCountTest() {
+		intMover.incCount(0);
+		assertEquals("Count the only zero",1,intMover.getCount(0));
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void doubleCountTest() {
+		intMover.incCount(0);
+		intMover.incCount(0);
+		assertEquals("Count the two zeroes",2,intMover.getCount(0));
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void randomCountTest() {
+		int rand = (int) Math.floor(Math.random() * 100);
+		for(int x = 0; x < rand; x++) {
+			intMover.incCount(0);
+		}
+		assertEquals("Count the randomized number of inserts",rand,intMover.getCount(0));
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void twoIncCountTest() {
+		intMover.incCount(0);
+		intMover.incCount(1);
+		assertEquals("Count the only zero when another number is inserted", 1 ,intMover.getCount(0));
+	}
+	
+	@Test (timeout = TIMEOUT)
+	public void hundredIncCountTest() {
+		for(int x = 0; x < 100; x++) {
+			intMover.incCount(0);
+		}
+		assertEquals("Count 100 zeroes!", 100 ,intMover.getCount(0));
+	}	
+	
+	
 }
